336f78a58688bc580b1d96b455eb10d0e9c1c56c
Sabrina Simkhovich <sabrinasimkhovich@gmail.com>
2024-03-17 09:42:28 -0500

    Added Player Class

--- Uno_Game.py
+++ Uno_Game.py
@@ -5,6 +5,29 @@ COLORS = ('red', 'yellow', 'blue', 'green')
 NUMBERS = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 # Add action cards later
 
+class Game:
+    '''
+    PARAM: number of players
+    '''
+    def __init__(self, num_player):
+        self.num_player = num_player
+        self.players = []
+
+    def new_game(self):
+        player_count = 0;
+        for player in self.num_player:
+            player_count +=1
+            self.players.append(Player(player_count))
+
+        
+class Player:
+    def __init__(self, number):
+        self.number = number
+        self.hand = []
+
+
+
+
 class Card:
     def __init__(self, color, number):
         self.color = color
@@ -16,7 +39,6 @@ class Card:
 class Hand:
     def __init__(self):
         self.cards = []
-        self.new_hand()  # Populate the hand with cards upon initialization
     
     def add_card(self):
         self.cards.append(random_card())
@@ -27,13 +49,44 @@ class Hand:
 
     def show_hand(self):
         for card in self.cards:
-            print(card)  # This will call the __str__ method of the Card class
+            print(card)  
     
-    def new_hand(self):
+    def new_hand(self, deck):
         for _ in range(7):  
-            self.add_card()  
+            self.add_card(deck.draw_card())  
         print('Here\'s your new hand!')
 
+class Deck:
+    def __init__(self):
+        self.deck = []
+        self.new_deck()
+        #deck is made up of 108 cards: 25 red, 25 blue, 25 green, 25 yellow. 
+        #2 each on 1-9 and 1 zero
+        #2 draw +2, 2 reverse, 2 skip, 4 wild, 4 wild draw four
+    def new_deck(self):
+        self.deck = []
+        for color in COLORS:
+            for number in NUMBERS:
+                # Adds two of each number for each color, except '0' which is added once
+                self.deck.append(Card(color, number))
+                if number != '0':
+                    self.deck.append(Card(color, number))
+    def show_deck(self):
+        for card in self.deck:
+            print(card)
+
+    def draw_card(self):
+        if len(self.deck) > 0:
+            return self.deck.pop()
+        else:
+            print("The deck is empty!")
+            return None
+
+    def shuffel_deck(self):
+        random.shuffle(self.deck)
+        print(type(self.deck))
+        
+    
 def random_card():
     # Returns a random card with a color and a number
     ranColor = random.choice(COLORS)
@@ -41,8 +94,17 @@ def random_card():
     return Card(ranColor, ranNumber)
 
 def main():
-    newHand = Hand()
-    newHand.show_hand()
+    # newHand = Hand()
+    # newHand.new_hand()
+    # newHand.show_hand()
+    # print("helo world")
+    newDeck = Deck()
+    # newDeck.show_deck()
+    newDeck.shuffel_deck()
+    # print(type(newDeck))
+    newDeck.show_deck()
+
+
 
 if __name__ == "__main__":
     main()
